20b63b929745dea7251343576f85a3683ac369b8
Evgeny Korostelev <ddc_smoke@mail.ru>
2024-02-13 09:32:10 +0300

    create_DiscordBot_and_joinHim_on_myDiscServer

--- Discord_Music_Bot/main.py
+++ Discord_Music_Bot/main.py
@@ -1,1 +1,13 @@
 # Discord_Bot_for_music
+from discord_components import ComponentsBot
+
+from music_cog import music_cog
+
+
+bot = ComponentsBot(command_prefix = '+')
+
+bot.add_cog(music_cog(bot))
+
+with open('D:/token.txt', 'r') as file:
+    token = file.readlines()[0]
+bot.run(token)
