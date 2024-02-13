cd55bfe68fef2f66cf499c86b38c3b18cfcd57d7
Evgeny Korostelev <ddc_smoke@mail.ru>
2024-02-13 19:17:06 +0300

    add_playFuncs_in_DiscMusicBot

--- Discord_Music_Bot/music_cog.py
+++ Discord_Music_Bot/music_cog.py
@@ -21,8 +21,14 @@ class music_cog(commands.Cog):
         self.YTDL_OPTIONS = {'format': 'bestaudio', 'nonplaylist': 'True'}
         self.FFMPEG_OPTIONS = {
             'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        
+        self.embedBlue = 0x2c76dd
+        self.embedRed = 0xdf1141
+        self.embedGreen = 0x0eaa51
 
         self.vc = {}
+
+#search and read       
     @commands.Cog.listener()
     async def on_ready(self):
         for guild in self.bot.guilds:
@@ -32,6 +38,21 @@ class music_cog(commands.Cog):
             self.vc[id] = None
             self.is_paused[id] = self.is_playing[id] = False
 
+    def now_playing_embed(self, ctx, song):
+        title = song['title']
+        link = song['link']
+        thumbnail = song['thumbnail']
+        author = ctx.author
+        avatar = author.avatar_url
+
+        embed = discord.Embed(
+            title = "Сейчас поёт:"
+            description = f'[{title}]({link})'
+            colour = self.embedBlue
+        )
+        embed.set_thumbail(url=thumbnail)
+        embed.set_footer(text=f'Песня добавлена ​​пользователем: {str(author)}, icon_url=avatar')
+        return embed
     async def join_VC(self, ctx, channel):
         id = int(ctx.guild.id)
         if self.vc[id] == None or self.vs[id].is_connected():
@@ -44,7 +65,7 @@ class music_cog(commands.Cog):
             await self.vc[id].move_to(channel)
 
     def search_YT(self, search):
-        queryString = parse.urlencode('search_query': search)
+        queryString = parse.urlencode({'search_query': search})
         htmlContent = request.urlopen('http://www.youtube.com/results?' + queryString)
         searchResults = re.findall('/watch\?v=(.{11})', htmlContent.read().decode())
         return searchResults[0:10]
@@ -59,4 +80,46 @@ class music_cog(commands.Cog):
             'thumbnail': 'https://i.yting.com/vi/' + url + '/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD5uL4xKN-IUfez6KIW_j5y70mlig',
             'source': info['formats'][0]['url'],
             'title': info['title']
-        }    
+        }
+#Playing    
+    def play_next(self, ctx):
+        id = int(ctx.guild.id)
+        if not self.is_playing[id]:
+            return
+        if self.queueIndex[id] + 1 < len(self.musicQueue[id]):
+            self.is_playing[id] = True
+            self.queueIndex[id] += 1
+
+            song = self.musicQueue[id][self.queueIndex[id]][0]
+            message = self.now_playing_embed(ctx, song)
+            coro = ctx.send(embed = message)
+            fut = run_coroutine_threadsafe(coro, self.bot.loop)
+            try:
+                fut.result()
+            except:
+                pass
+
+            self.vc[id].play(discord.FFmpegPCMAudio(
+                song['source'], **self.FFMPEG_OPTIONS), after = lambda e: self.play_next(ctx))
+        else:
+            self.queueIndex[id] += 1
+            self.is_playing[id] = False
+
+    async def play_music(self, ctx):
+        id = int(ctx.guild.id)
+        if self.queueIndex[id] < len(self.musicQueue[id]):
+            self.is_playing = True
+            self.is_paused = False
+
+            await self.join_VC(ctx, self.musicQueue[id][self.queueIndex[id]][1])
+
+            song = self.musicQueue[id][self.queueIndex[id]][0]
+            message = self.now_playing_embed(ctx, song)
+            await ctx.send(embed = message)
+
+            self.vc[id].play(discord.FFmpegPCMAudio(
+                song['source'], **self.FFMPEG_OPTIONS), after = lambda e: self.play_next(ctx))
+        else:
+            await ctx.send("В очереди на воспроизведение нет песен!")
+            self.queueIndex[id] += 1
+            self.is_playing[id] = False
