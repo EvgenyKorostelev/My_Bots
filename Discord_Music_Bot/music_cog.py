820c7197ef81d1391af72f7d91d4177ae003fc0c
Evgeny Korostelev <ddc_smoke@mail.ru>
2024-02-13 10:29:21 +0300

    add_YoutubeFuncs_in_music_cog

--- Discord_Music_Bot/music_cog.py
+++ Discord_Music_Bot/music_cog.py
@@ -12,3 +12,51 @@ from youtube_dl import YoutubeDL
 class music_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+        self.is_playing = {}
+        self.is_paused = {}
+        self.musicQueue = {}
+        self.queueIndex = {}
+
+        self.YTDL_OPTIONS = {'format': 'bestaudio', 'nonplaylist': 'True'}
+        self.FFMPEG_OPTIONS = {
+            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+        self.vc = {}
+    @commands.Cog.listener()
+    async def on_ready(self):
+        for guild in self.bot.guilds:
+            id = int(guild.id)
+            self.musicQueue[id] = []
+            self.queueIndex[id] = 0
+            self.vc[id] = None
+            self.is_paused[id] = self.is_playing[id] = False
+
+    async def join_VC(self, ctx, channel):
+        id = int(ctx.guild.id)
+        if self.vc[id] == None or self.vs[id].is_connected():
+            self.vc[id] = await channel.connect()
+
+            if self.vc[id] == None:
+                await ctx.send("Не удалось подключится к голосовому каналу!")
+                return
+        else:
+            await self.vc[id].move_to(channel)
+
+    def search_YT(self, search):
+        queryString = parse.urlencode('search_query': search)
+        htmlContent = request.urlopen('http://www.youtube.com/results?' + queryString)
+        searchResults = re.findall('/watch\?v=(.{11})', htmlContent.read().decode())
+        return searchResults[0:10]
+    def extract_YT(self, url):
+        with YoutubeDL(self.YTDL_OPTIONS) as ydl:
+            try:
+                info =ydl.extract_info(url, download=False)
+            except:
+                return False
+        return {
+            'link': 'http://www.youtube.com/watch?v=' + url,
+            'thumbnail': 'https://i.yting.com/vi/' + url + '/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD5uL4xKN-IUfez6KIW_j5y70mlig',
+            'source': info['formats'][0]['url'],
+            'title': info['title']
+        }    
